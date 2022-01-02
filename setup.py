b5a935ac12087d19ab5e130d4f1d101b830de361
s22215 <81318715+s22215@users.noreply.github.com>
2022-01-02 20:55:03 +0100

    Fixed main package name.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='',
    author='Michal Pilichowski',
    author_email='',
-   packages=['pilichm.main'],
+   packages=['main'],
    install_requires=['spacy==2.3.2', 'keras', 'numpy', 'pandas', 'tensorflow', 'matplotlib', 'sklearn', 'seaborn']
 )
