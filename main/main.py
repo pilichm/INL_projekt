808039e6aedc8ce978e4b196747671a2f3330f72
s22215 <81318715+s22215@users.noreply.github.com>
2022-01-04 19:43:17 +0100

    Added main colab file.

--- main/main.py
+++ main/main.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     start_time = time.time()
 
     try:
-        # run_crf(TRAIN_DATASET, TEST_DATASET, epoch_count=1000, display_diagrams=True, mode='all')
+        run_crf(TRAIN_DATASET, TEST_DATASET, epoch_count=1000, display_diagrams=True, mode='all_but_word_morf')
 
-        run_nn(TRAIN_DATASET, TEST_DATASET, model_name='dense', epoch_count=25, display_diagrams=True)
+        # run_nn(TRAIN_DATASET, TEST_DATASET, model_name='dense', epoch_count=25, display_diagrams=True)
 
         # run_logistic_regression(TRAIN_DATASET, TEST_DATASET, max_iteration_count=2_000, display_diagrams=True)
 
